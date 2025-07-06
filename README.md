acdc08111ea03a0d1fe313d1905e6eb233e20d6f
mikezhylka <applezhylka@gmail.com>
2025-07-06 15:49:55 +0200

    feat: initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+Figma design
+
+https://www.figma.com/design/n4YNrWjmWa3B6cM8LrtTn7/Phone-Catalog?node-id=0-1&m=dev
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="p-6 max-w-xl mx-auto bg-gray-100 rounded-lg shadow-md">
+      <h1 className="font-t text-4xl text-blue-600 mb-4">Product Catalog</h1>
+      <p className="text-gray-700 mb-6">
+        Explore our amazing selection of products.
+      </p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Shop Now
+      </button>
+    </div>
+  );
+}
+
+export default App;
