d0d43225244b871073715ebbfb4657802b8ec121
mikezhylka <applezhylka@gmail.com>
2025-11-30 21:04:37 +0100

    fix: add api folder inside src

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,9 +1,9 @@
-import accessories from "../../public/api/accessories.json";
-import phones from "../../public/api/phones.json";
-import tablets from "../../public/api/tablets.json";
+import accessories from "../api/accessories.json";
+import phones from "../api/phones.json";
+import tablets from "../api/tablets.json";
 
 export const categories = {
-  phones: phones,
-  tablets: tablets,
-  accessories: accessories,
+  phones,
+  tablets,
+  accessories,
 };
--- src/utils/sortProducts.ts
+++ src/utils/sortProducts.ts
@@ -1,4 +1,4 @@
-import products from "../../public/api/products.json";
+import products from "../api/products.json";
 
 export const sortProducts = (sortBy: "year" | "prices") => {
   return [...products].sort((p1, p2) => {
